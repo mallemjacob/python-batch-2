78cdf8a45cc713507796e27ecf84e76b01bd0b9a
jakejacobs2015 <jacky4uonly2009@gmail.com>
2025-09-19 16:49:07 +0530

    initial commit

---
+++ basics.py
@@ -0,0 +1,59 @@
+# This is my first python program
+# writte on septmener 1, 2025
+# written by me
+
+#data types
+#strings
+#integers
+#float
+#boolean
+
+#operators
+#arthemetic operator --> +, -, /, //, **, %, *
+#string operator --> +, *
+#comparision operators --> >, < >=, <=, ==, !=
+#boolean operators --> and, or, not
+
+print(2 + 4) #addition
+print(4 - 2) #subtraction
+print( 5 / 2) #division
+print( 5 // 2) #integer division
+print( 3 * 3) #multiplication
+print( 2 ** 4) #power
+print(10 % 5) # modulus
+
+print('hi ' + 'there') # concatenation
+print('hi ' * 4) # replcation
+
+
+print( 2 > 1) #True
+print( 10 < 5 ) #False
+print( 2 >= 2 ) #True
+print( 5 <= 3) #False
+print( 10 == 10) #True
+print( 10 != 10) #False
+
+#boolean
+#and 
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+#or
+print(True or True)
+print("Line 46:")
+print(2 > 1 or  10 < 5)
+print(True or False)
+print(False or True)
+print(False or False)
+
+#not
+print('line 52: ')
+print(not True)
+print(not False)
+
+print('hi' == 'hi')
+print('hi' == 'bye')
+print('ashok' != 'ashok')
+print('priyanka' != 'ashok')
