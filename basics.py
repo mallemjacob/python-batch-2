1144dba4820cfa79b7e7490be05f85efaa344897
jakejacobs2015 <jacky4uonly2009@gmail.com>
2025-09-22 15:45:32 +0530

    Add If statements

--- basics.py
+++ basics.py
@@ -14,46 +14,97 @@
 #comparision operators --> >, < >=, <=, ==, !=
 #boolean operators --> and, or, not
 
-print(2 + 4) #addition
-print(4 - 2) #subtraction
-print( 5 / 2) #division
-print( 5 // 2) #integer division
-print( 3 * 3) #multiplication
-print( 2 ** 4) #power
-print(10 % 5) # modulus
+# print(2 + 4) #addition
+# print(4 - 2) #subtraction
+# print( 5 / 2) #division
+# print( 5 // 2) #integer division
+# print( 3 * 3) #multiplication
+# print( 2 ** 4) #power
+# print(10 % 5) # modulus
 
-print('hi ' + 'there') # concatenation
-print('hi ' * 4) # replcation
+# print('hi ' + 'there') # concatenation
+# print('hi ' * 4) # replcation
 
 
-print( 2 > 1) #True
-print( 10 < 5 ) #False
-print( 2 >= 2 ) #True
-print( 5 <= 3) #False
-print( 10 == 10) #True
-print( 10 != 10) #False
+# print( 2 > 1) #True
+# print( 10 < 5 ) #False
+# print( 2 >= 2 ) #True
+# print( 5 <= 3) #False
+# print( 10 == 10) #True
+# print( 10 != 10) #False
 
-#boolean
-#and 
-print(True and True)
-print(True and False)
-print(False and True)
-print(False and False)
+# #boolean
+# #and 
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
 
-#or
-print(True or True)
-print("Line 46:")
-print(2 > 1 or  10 < 5)
-print(True or False)
-print(False or True)
-print(False or False)
+# #or
+# print(True or True)
+# print("Line 46:")
+# print(2 > 1 or  10 < 5)
+# print(True or False)
+# print(False or True)
+# print(False or False)
 
-#not
-print('line 52: ')
-print(not True)
-print(not False)
+# #not
+# print('line 52: ')
+# print(not True)
+# print(not False)
 
-print('hi' == 'hi')
-print('hi' == 'bye')
-print('ashok' != 'ashok')
-print('priyanka' != 'ashok')
+# print('hi' == 'hi')
+# print('hi' == 'bye')
+# print('ashok' != 'ashok')
+# print('priyanka' != 'ashok')
+
+#if statements
+
+# print('Enter your name:')
+# name = input() #bill
+
+# if name == 'hello':
+#     print('this is hello')
+#     print('welcome')
+# elif name == 'bye':
+#     print('this is hello')
+# else:
+#     print('False')
+#     age = int(input()) # '18' ---> 18
+#     if age == 18:
+#         print('you are an adult')
+#         print('you can vote')
+
+
+# if 2 > 1:
+#     if 2 == 3:
+#         print('nested if')
+#     else:
+#         print('inner false')
+# else:
+#     print('false')
+
+# name = 'Carol'
+# age = 3000
+# if age == 4000: #3000 == 4000
+#     print('Hi, Alice.')
+# elif age == 5000: # 3000 == 5000
+#     print('vampire')
+# else:
+#     print('hi grannie')
+
+# This program says hello and asks for my name.
+# print('Hello, world!')
+# print('What is your name?')
+# ask for their name
+# myName = input() #helloworld --> 10
+# if myName == 'valkyrie':
+#     print('It is good to meet you, ' + myName)
+#     print('The length of your name is:')
+#     print(len(myName))
+#     if len(myName) > 5:
+#         print('What is your age?')
+#         myAge = input() #'19' ---> 19
+#         print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+#     else:
+#         print('The length of name must be greater than 5 characters.')
